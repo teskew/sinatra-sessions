ad22fce7c00afa6a059cc807bde65cd31e54e4cd
teskew <teskinder1@gmail.com>
2021-02-15 12:45:59 -0600

    commit tree 4b1dbd273eb9e98ffa5aa437b78483563933752e

--- app.rb
+++ app.rb
@@ -20,6 +20,7 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
+    session[:foo] = "hello"
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
@@ -37,9 +38,10 @@ class App < Sinatra::Base
 
   get '/set_session' do
     #set session id here
-
+      session[:id] = 1
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
+
       redirect '/fetch_session_id'
     else
       "Session ID has not been set!"
@@ -50,12 +52,15 @@ class App < Sinatra::Base
     "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
   end
 
+
   get '/logout' do
     #clear session hash here
+    session.clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
   get '/finish' do
+  
     "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
   end
 end
